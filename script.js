dd7ae873a7bef5519a612b8679f57151fea80fe8
BGWEB <121228936+BGWEB08@users.noreply.github.com>
2024-01-13 23:15:55 +0300

    script.js

--- script.js
+++ script.js
@@ -3,20 +3,5 @@ function togglebtn() {
   navBar.classList.toggle("hidemenu");
 }
 
-// single product
-let mainImage = document.getElementById("main-image");
-let smallImage = document.getElementsByClassName("image");
-
-smallImage[0].onclick = () => {
-  mainImage.src = smallImage[0].src;
-};
-
-smallImage[1].onclick = () => {
-  mainImage.src = smallImage[1].src;
-};
-
-smallImage[2].onclick = () => {
-  mainImage.src = smallImage[2].src;
-};
 
 
